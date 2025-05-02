04d9c615123e87e7d69cdbf099f308bd6e825cff
Roman <romancheprack@gmail.com>
2025-05-03 00:39:03 +0300

    Adding hEAD to app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])  # only GET
+@app.route("/", methods=["GET", "HEAD"])  # only GET
 def index():
     return render_template("index.html")
 
